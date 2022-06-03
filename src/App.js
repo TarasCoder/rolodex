cad2aa1191500a3082c5beb9d8d168d619725bfe
TarasCoder <4kaskad@gmail.com>
2022-06-03 20:25:12 +0300

    adding fetch functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const search = (ev) => {
+    console.log(ev.target.value);
+  };
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setData(data);
+    });
+
   return (
     <div className="App">
-      
+      <input type="search" placeholder="Enter name:" onChange={search} />
+      {data.map((item) => {
+        return (
+          <div key={item.id}>
+            <h3>{item.name}</h3>
+          </div>
+        );
+      })}
     </div>
   );
 }
