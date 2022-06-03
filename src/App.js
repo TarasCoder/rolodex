664dbe07e3424d2383b8f205f628e88c89bb45e5
unknown <4kaskad@gmail.com>
2022-06-03 22:51:58 +0300

    adding filtering functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  let filtered = data.filter((item) => {
+    return item.name.toLowerCase().includes(input.toLowerCase());
+  });
 
   const search = (ev) => {
-    console.log(ev.target.value);
+    setInput(ev.target.value);
   };
-
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setData(data);
-    });
 
   return (
     <div className="App">
       <input type="search" placeholder="Enter name:" onChange={search} />
-      {data.map((item) => {
+      {filtered.map((item) => {
         return (
           <div key={item.id}>
             <h3>{item.name}</h3>
